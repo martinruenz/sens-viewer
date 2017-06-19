ca54c650d55c664ef7e2f3a5851ea0a5ba87af26
Martin Rünz <martin.ruenz@posteo.de>
2017-06-19 13:25:32 +0100

    Tool works now.

--- main.cpp
+++ main.cpp
@@ -15,92 +15,62 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 *****************************************************************/
 
+#include <cstddef>
+#include <iostream>
 #include "external/sensorData.h"
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace std;
-//using namespace cv;
-
-int main(int argc, char * argv[])
-{
-
-
-    return 0;
-}
-
-
-//THIS IS A DEMO FUNCTION: HOW TO DECODE .SENS FILES: CHECK IT OUT! (doesn't do anything real though)
-void processFrame(const ml::SensorData& sd, size_t frameIdx) {
-
-    //de-compress color and depth values
-    ml::vec3uc* colorData = sd.decompressColorAlloc(frameIdx);
-    unsigned short* depthData = sd.decompressDepthAlloc(frameIdx);
-
-    //dimensions of a color/depth frame
-    sd.m_colorWidth;
-    sd.m_colorHeight;
-    sd.m_depthWidth;
-    sd.m_depthHeight;
-
-    for (unsigned int i = 0; i < sd.m_depthWidth * sd.m_depthHeight; i++) {
-        //convert depth values to m:
-        float depth_in_meters = sd.m_depthShift * depthData[i];
-    }
-
-    std::free(colorData);
-    std::free(depthData);
-}
-
+using namespace cv;
 
 int main(int argc, char* argv[])
 {
 
     try {
         //non-cached read
-        std::string filename = "scene0001_00.sens";
-        std::string outDir = "./out/";
-        if (argc >= 2) filename = std::string(argv[1]);
+        string filename;
+        if (argc >= 2) filename = string(argv[1]);
         else {
-            std::cout << "run ./sens <sensfilename>.sens";
-            std::cout << "type in filename manually: ";
-            std::cin >> filename;
+            cout << "use ./sens-viewer <filename>" << endl;
+            return 1;
         }
-        if (argc >= 3) outDir = std::string(argv[2]);
 
-        std::cout << "filename =\t" << filename << std::endl;
-        std::cout << "outDir =\t" << outDir << std::endl;
-
-        std::cout << "loading from file... ";
+        cout << "Loading data ... ";
         ml::SensorData sd(filename);
-        std::cout << "done!" << std::endl;
+        cout << "done!" << endl;
+        cout << sd << endl;
 
-        std::cout << sd << std::endl;
+        for (size_t i = 0; i < sd.m_frames.size(); i++) {
 
-        // color frames are written out as 'jpgs' and depth frames as binary dumps: <width : uint32, height : uint32, unsigned short* data>
-        sd.saveToImages(outDir);
+            //de-compress color and depth values
+            ml::vec3uc* colorData = sd.decompressColorAlloc(i);
+            unsigned short* depthData = sd.decompressDepthAlloc(i);
 
-        //THIS SHOWS HOW DE-COMPRESSION WORKS
-        //for (size_t i = 0; i < sd.m_frames.size(); i++) {
-        {
-            size_t i = 0;
-            //std::cout << "\r[ processing frame " << std::to_string(i) << " of " << std::to_string(sd.m_frames.size()) << " ]";
-            processFrame(sd, i);
+            Mat depth_raw(sd.m_depthHeight, sd.m_depthWidth, CV_16UC1, depthData);
+            Mat rgb(sd.m_colorHeight, sd.m_colorWidth, CV_8UC3, (unsigned char*)colorData);
+            cvtColor(rgb, rgb, CV_BGR2RGB);
+            Mat depth;
+            depth_raw.convertTo(depth, CV_16UC1, 20);
+
+            cv::imshow("Depth", depth);
+            cv::waitKey(1);
+            cv::imshow("RGB", rgb);
+            cv::waitKey(1);
+
+            free(colorData);
+            free(depthData);
         }
 
-
-        std::cout << std::endl;
-    }
-    catch (const std::exception& e)
-    {
-        std::cout << "Exception " << e.what() << std::endl;
+        cout << endl;
+    }catch (const exception& e){
+        cout << "Exception " << e.what() << endl;
+        return 1;
+    }catch (...){
+        cout << "Exception (unknown)";
         return 1;
     }
-    catch (...)
-    {
-        std::cout << "Exception (unknown)";
-        return 1;
-    }
-
-    std::cout << "All done :)" << std::endl;
 
     return 0;
 }
